1ecdb41ecdc72e4b2e2eb35b2ae64e192dfe1231
Jeevan Kondaveeti <kondaveetijeevan@gmail.com>
2024-08-23 07:37:14 +0530

    add_notes

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask,redirect,render_template,request,url_for,flash
+from flask import Flask,redirect,render_template,request,url_for,flash,session
+from flask_session import Session
 from colorama import Fore, Back, Style
 from otp import genotp
 from spmmail import semdmail
 import mysql.connector
 from stoken  import token,dtoken
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 app.secret_key = "E11CD7B8D64CAA25611DD4EE22393"
 
 mydb = mysql.connector.connect(user='root',password='admin',db='SPM',host='localhost')
@@ -43,7 +46,8 @@ def register():
             """
             return 'mail  already exist'
          cursor = mydb.cursor(buffered=True)
-        data=cursor.execute('insert into student (email,student_fname,student_lname,password) values (%s,%s,%s,%s)',[s_mail,s_fname,s_lname,s_pass])
+        data=cursor.execute('insert into student (email,student_fname,student_lname,password) 
+        values (%s,%s,%s,%s)',[s_mail,s_fname,s_lname,s_pass])
         print(data)
         mydb.commit()
         cursor.close() """
@@ -63,7 +67,10 @@ def otp_verification(data1):
             uotp = request.form['otp'] 
             if uotp == data['otp'] :
                 cursor = mydb.cursor(buffered=True)
-                data=cursor.execute('insert into student (email,student_fname,student_lname,password) values (%s,%s,%s,%s)',[s_mail,s_fname,s_lname,s_pass])
+                data=cursor.execute(
+                    'insert into student (email,student_fname,student_lname,password) values (%s,%s,%s,%s)',
+                    [s_mail,s_fname,s_lname,s_pass]
+                    )
                 print(data)
                 mydb.commit()
                 cursor.close()
@@ -84,34 +91,82 @@ def otp_verification(data1):
 
 @app.route('/Login',methods=['GET','POST'])
 def Login():
-    if request.method == 'POST':
-        login_info = dict(request.form)
-        email = login_info['email']
-        passwrd = login_info['password']
-        print([email,passwrd])
-        try:
-            cursor = mydb.cursor(buffered=True)
-            cursor.execute('select email,password from student where email = %s',[email])
-            data = cursor.fetchone()
-            passkey = data[1].decode('utf-8')
-            print(type(passkey))
-        except Exception as e:
-            print(e)
-            flash("Please check email")
-            return redirect(url_for('Login'))
-        else:
-            if passkey == passwrd:
-                return render_template('panel.html')
+    if session.get('email'):
+        return redirect(url_for('panel'))
+    else:
+        if request.method == 'POST':
+            login_info = dict(request.form)
+            email = login_info['email']
+            passwrd = login_info['password']
+            print([email,passwrd])
+            try:
+                cursor = mydb.cursor(buffered=True)
+                cursor.execute('select email,password from student where email = %s',[email])
+                data = cursor.fetchone()
+                passkey = data[1].decode('utf-8')
+                print(type(passkey))
+            except Exception as e:
+                print(e)
+                flash("Please check email")
+                return redirect(url_for('Login'))
             else:
-                flash("Invalid Password")
-                
-                
+                if passkey == passwrd:
+                    session['email'] = email
+                    if not session.get(email):
+                        session[email]={}
+                        return render_template('panel.html')
+                else:
+                    flash("Invalid Password")            
     return render_template("Login.html")
 @app.route('/addNote',methods = ['GET','POST'])
 def addNotes():
-    if request.method == 'POST':
-        notes = dict(request.form)
-        print(notes)
-        return "ok"
+    if not session.get('email'):
+        return redirect(url_for('Login'))
+    else:
+        if request.method == 'POST':
+            notes = dict(request.form)
+            tittle = notes['Notes_Title']
+            content = notes['content']
+            addedby = session.get('email')
+            cursor = mydb.cursor(buffered=True)
+            cursor.execute(
+                'insert into notes(title,note_content,added_by) values (%s,%s,%s)',[tittle,content,addedby]
+            )
+            mydb.commit()
+            cursor.close()
+            flash(f'Notes with {tittle} added successfully')
+            return render_template('add_notes.html')
     return render_template('add_notes.html')
+
+@app.route('/panel')
+def panel():
+    if not session.get('email'):
+        return render_template('Login.html')
+    return render_template('panel.html')
+
+@app.route('/updatenotes',methods=['GET','POST'])
+def updatenotes():
+    if not session.get('email'):
+        return render_template('Login.html')
+    return render_template('updatenotes.html')
+
+@app.route('/allnotes')
+def allnotes():
+    if not session.get('email'):
+        return render_template('Login.html')
+    added_by = session.get('email')
+    cursor = mydb.cursor(buffered=True)
+    cursor.execute("select created_at,nid,title from notes where added_by=%s",[added_by])
+    data = cursor.fetchall()
+    print(data)
+    return render_template('table.html',data=data)
+
+@app.route('/Logout')
+def logout():
+    if session.get('email'):
+        session.pop('email')
+        return redirect(url_for('Login'))
+    else:
+        return redirect(url_for('Login'))
+
 app.run(debug=True,host='localhost',port=1601,use_reloader=True)
